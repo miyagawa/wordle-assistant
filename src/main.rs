0879b3bff37942fcabccf937023b5720c589dadf
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2024-08-20 22:34:03 -0700

    use regex

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use std::error;
 use std::fs::File;
 use std::io::{self, BufRead};
+use regex::Regex;
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 fn read_words_file() -> Result<Vec<String>> {
     let file = File::open("/usr/share/dict/words")?;
 
-    let words = io::BufReader::new(file).lines()
-        .flatten()
-        .filter_map(|word| {
-            match (word.len(), word.chars().nth(0).unwrap()) {
-                (5, 'a'..='z') => Some(word),
-                _ => None,
-            }
-        })
-        .collect();
-
-    Ok(words)
+    Ok(io::BufReader::new(file).lines()
+       .flatten()
+       .filter_map(|word| {
+           match (word.len(), word.chars().nth(0).unwrap()) {
+               (5, 'a'..='z') => Some(word),
+               _ => None,
+           }
+       })
+       .collect())
 }
 
 #[derive(Debug)]
@@ -27,17 +26,13 @@ struct Guess {
 }
 
 fn satisfy(answer: &str, guess: &Guess) -> bool {
-    let mut status = String::new();
-    for (i, c) in guess.word.chars().enumerate() {
-        let s = if answer.chars().nth(i) == Some(c) {
-            'G'
-        } else if answer.contains(c) && answer.chars().nth(i) != Some(c) {
-            'Y'
-        } else {
-            'B'
-        };
-        status.push(s);
-    }
+    let status: String = guess.word.chars().enumerate().map(|(i, c)| {
+        match (answer.contains(c), answer.chars().nth(i)) {
+            (true, Some(a)) if a == c => 'G',
+            (true, Some(a)) if a != c => 'Y',
+            _ => 'B',
+        }
+    }).collect();
 
     status == guess.status
 }
@@ -46,29 +41,25 @@ fn main() {
     let words = read_words_file()
         .expect("Could not read file");
 
-    let mut guesses = vec![];
+    let re = Regex::new(r"^([a-zA-Z]{5}) ([YGB]{5})$").unwrap();
+    let mut guesses = Vec::new();
 
     loop {
         println!("---> Enter your guess, followed by YGB (Yellow, Green, Black)");
         let mut line = String::new();
-        match io::stdin().read_line(& mut line) {
-            Ok(0) => {
-                std::process::exit(0);
-            },
-            Ok(_) => {
-                let guess : Vec<&str> = line.split_whitespace().collect();
-                match guess.len() {
-                    2 => {
-                        guesses.push(Guess { word: guess[0].to_string(), status: guess[1].to_string() });
-                    },
-                    _ => {
-                        eprintln!("Invalid input.");
-                        continue;
-                    },
-                };
-            },
-            _ => {},
-        };
+        let n = io::stdin().read_line(&mut line);
+        if let Ok(0) = n {
+            std::process::exit(0);
+        }
+        if let Some(caps) = re.captures(&line.trim_end()) {
+            let word = caps.get(1).map_or("", |m| m.as_str()).to_string();
+            let status = caps.get(2).map_or("", |m| m.as_str()).to_string();
+
+            guesses.push(Guess { word, status });
+        } else {
+            eprintln!("Invalid input.");
+            continue;
+        }
 
         words.iter()
             .filter(|&word| guesses.iter().all(|guess| satisfy(word, guess)))
